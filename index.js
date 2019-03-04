f6cdb9e77756a942a82950eef1c861934bcd8184
david <david@adyoulike.com>
2019-03-04 15:53:34 +0100

    website-scrapper plugin

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ const nameGenerator = require('./modules/folder_name_generator.js');
 const puppeteer     = require('puppeteer');
 
 
-/* TODO
-NOTES :
-https://fettblog.eu/scraping-with-puppeteer/
-
-https://www.nytimes.com/
--> Blank
-
-https://www.urbanlife.de/
-https://www.wallstreet-online.de/
-https://www.gala.fr/l_actu/news_de_stars/alain-fabien-delon-mon-pere-est-bien-plus-proche-de-ma-soeur-anouchka-que-danthony-et-de-moi_425351
--> no images
-*/
-
-
 var assets = {
   dest_folder: nameGenerator(),
 };
@@ -30,9 +16,9 @@ var options = {
   urls: [args.u],
   directory: `./websites/${assets.dest_folder}`,
   plugins: [{
-      beforeStart: (async () => {
+      beforeStart: async () => {
         browser = await puppeteer.launch({headless: true});
-      })(),
+      },
       afterResponse: async (response) => {
         console.log(response);
         const contentType = response.headers['content-type'];
@@ -48,9 +34,9 @@ var options = {
   				return response.body;
   			}
       },
-      afterFinish: (async () => {
+      afterFinish: async () => {
         browser.close()
-      })()
+      }
     }]
 };
 
